70150703cb9b4442f6ecb584d8ecaf9c2d191964
Sihong Huang <sihongspring2011@gmail.com>
2019-11-22 18:25:57 -0500

    sets, maps

--- s8/setsmaps.js
+++ s8/setsmaps.js
@@ -1,10 +1,47 @@
-const ids = new Set(['Hi', 'from', 'set']);
-ids.add(2);
-if (ids.has('Hi')) {
-    ids.delete('Hi');
-}
-console.log(ids);
+// const ids = new Set(['Hi', 'from', 'set']);
+// ids.add(2);
+// if (ids.has('Hi')) {
+//     ids.delete('Hi');
+// }
+// console.log(ids);
 
-for (const entry of ids.entries()) {
-    console.log(entry);
-};
+// for (const entry of ids.entries()) {
+//     console.log(entry);
+// };
+
+// const person1 = {name: 'Max'};
+// const person2 = {name: 'Manuel'};
+
+// const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]]);
+
+// personData.set(person2, [{date: 'two weeks ago', price: 100}]);
+
+// console.log(personData);
+// console.log(personData.get(person1));
+
+// for (const entry of personData.entries()) {
+//     console.log(entry);
+// }
+
+// for (const key of personData.keys()) {
+//     console.log(key);
+// }
+
+// for (const value of personData.values()) {
+//     console.log(value);
+// }
+
+let person = {name: 'Max'};
+const persons = new WeakSet();
+persons.add(person);
+
+// person = null;
+
+console.log(persons);
+
+const personData = new WeakMap();
+personData.set(person, 'Extra info!');
+
+person = null;
+
+console.log(personData);
