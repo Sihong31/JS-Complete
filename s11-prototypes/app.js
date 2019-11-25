c0d5147774bcd68050f5388228f28222c31a31fb
Sihong Huang <sihongspring2011@gmail.com>
2019-11-25 06:32:13 -0500

    fix s11

--- s11-prototypes/app.js
+++ s11-prototypes/app.js
@@ -4,39 +4,110 @@ class AgedPerson {
   }
 }
 
-class Person extends AgedPerson {
-  name = 'Bob';
+class Person {
+  name = 'Max';
 
   constructor() {
-    super();
+    // super();
     this.age = 30;
+    // this.greet = function() { ... }
   }
+
+  // greet = () => {
+  //   console.log(
+  //     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+  //   );
+  // };
 
   greet() {
-    console.log('Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.')
+    console.log(
+      'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+    );
   }
 }
 
-function Person() {
-  this.name = 'Bob';
-  this.age = 30;
-}
-
-Person.prototype.greet = function() {
-  console.log(
-    'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-  )
-}
-
-// Person.prototype.printAge = function() {
-//     console.log(this.age);
+// function Person() {
+//   this.age = 30;
+//   this.name = 'Max';
+//   // this.greet = function() { ... };
 // }
 
-// const person = new Person();
-// person.greet();
-// person.printAge();
-// console.log(person.__proto__);
+// Person.prototype.greet = function() {
+//   console.log(
+//     'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//   );
+// };
 
-const p = new Person();
-const p2 = new Person();
-console.log(p.__proto__ === p2.__proto__);
+// Person.describe = function() {
+//   console.log('Creating persons...');
+// }
+
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// };
+
+// Person.prototype.printAge = function() {
+//   console.log(this.age);
+// };
+
+// console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.length);
+// console.log(p.toString());
+// const p2 = new p.__proto__.constructor();
+// console.dir(Object.prototype.__proto__);
+
+// const p = new Person();
+// const p2 = new Person();
+// p.greet();
+// console.log(p);
+
+// const button = document.getElementById('btn');
+// button.addEventListener('click', p.greet.bind(p));
+
+const course = {
+  // new Object()
+  title: 'JavaScript - The Complete Guide',
+  rating: 5
+};
+
+// console.log(Object.getPrototypeOf(course));
+Object.setPrototypeOf(course, {
+  // ...Object.getPrototypeOf(course),
+  printRating: function() {
+    console.log(`${this.rating}/5`);
+  }
+});
+
+const student = Object.create({
+  printProgress: function() {
+    console.log(this.progress);
+  }
+}, {
+  name: {
+    configurable: true,
+    enumerable: true,
+    value: 'Max',
+    writable: true
+  }
+});
+
+// student.name = 'Max';
+
+Object.defineProperty(student, 'progress', {
+  configurable: true,
+  enumerable: true,
+  value: 0.8,
+  writable: false
+});
+
+student.printProgress();
+
+console.log(student);
+
+course.printRating();
