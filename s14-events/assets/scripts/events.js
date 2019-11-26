c5c023c8403ea1520a56c55c599af43c99b0c1bc
Huang <sihong.huang@accenture.com>
2019-11-26 16:33:53 -0500

    playing with events

--- s14-events/assets/scripts/events.js
+++ s14-events/assets/scripts/events.js
@@ -1,7 +1,7 @@
-const buttons = document.querySelectorAll('button');
+const button = document.querySelector('button');
 
 const buttonClickHandler = event => {
-  event.target.disabled = true;
+  // event.target.disabled = true;
   console.log(event);
 }
 
@@ -25,6 +25,48 @@ const boundFn = buttonClickHandler.bind(this);
 //   button.removeEventListener('click', boundFn);
 // }, 2000);
 
-buttons.forEach(btn => {
-  btn.addEventListener('click', buttonClickHandler);
+// buttons.forEach(btn => {
+//   btn.addEventListener('mouseenter', buttonClickHandler);
+// });
+
+// window.addEventListener('scroll', event => {
+//   console.log(event);
+// });
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event);
+});
+
+const div = document.querySelector('div');
+
+div.addEventListener('click', event => {
+  console.log('CLICKED DIV', event);
+});
+
+button.addEventListener('click', function(event) {
+  event.stopPropagation();
+  console.log('CLICKED BUTTON', event);
+  console.log(event);
+  console.log(this);
+});
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//   listItem.addEventListener('click', event => {
+//     event.target.classList.toggle('highlight');
+//   });
+// })
+
+list.addEventListener('click', function(event) {
+  // console.log(event.currentTarget);  
+  // event.target.classList.toggle('highlight');
+  event.target.closest('li').classList.toggle('highlight');
+  // form.submit();
+  button.click();
+  console.log(this);
 });
